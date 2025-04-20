4a3fe6931e19c787d1affe2d9e008a1dd67afde0
aloksati7 <121572262+aloksati4@users.noreply.github.com>
2025-04-20 10:30:54 +0530

    first question

---
+++ Linked-List/Middle_of_linked_list.java
@@ -0,0 +1,13 @@
+// This is leetcode 876
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+}
